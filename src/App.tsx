5f41353d7147e01d25fb8e2da351737e3dba04aa
zakky <sadaoza@gmail.com>
2024-06-17 16:55:34 +0900

    Call Custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,13 @@ import { useState } from "react";
 import axios from "axios";
 
 import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/UserProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 import "./App.css";
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city} ${user.address.suite} ${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const onClickFetchUser = () => getUsers();
 
   return (
     <div className="App">
